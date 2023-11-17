75b0c29879ef657322909bda5fb91bcb2b71b52b
GianlucaMinoprio <58565032+GianlucaMinoprio@users.noreply.github.com>
2023-11-18 02:04:23 +0300

    EncryptionTestScreen

--- frontend/SoundSafeApp/app/screens/EncryptionTestScreen.tsx
+++ frontend/SoundSafeApp/app/screens/EncryptionTestScreen.tsx
@@ -10,48 +10,72 @@ import {
 } from "react-native-secure-encryption-module"
 
 const EncryptionTestScreen: React.FC = () => {
-  const testEncryptionModule = async () => {
+  const keyAlias = "my-key"
+  let publicKey: string = ""
+  let cipher: string = ""
+  let signature: string = ""
+
+  const handleGenerateKeyPair = async () => {
     try {
-      const keyAlias = "my-key"
-
-      // Check hardware support
-      const isSupported = await isKeySecuredOnHardware(keyAlias)
-      if (!isSupported) {
-        Alert.alert("Unsupported", "Hardware level security is not available on this device.")
-        return
-      }
-
-      // Generate key pair
-      const publicKey = await generateKeyPair(keyAlias)
-
-      // Encrypt a message
-      const message = "Hello World"
-      const cipher = await encrypt(message, keyAlias)
-
-      // Sign the message
-      const signature = await sign(message, keyAlias)
-
-      // Decrypt the message
-      const decryptedMessage = await decrypt(cipher, keyAlias)
-
-      // Verify the signature
-      const isSignatureValid = await verify(signature, message, keyAlias)
-
-      // Display results
-      Alert.alert(
-        "Test Results",
-        `Public Key: ${publicKey}\nDecrypted Message: ${decryptedMessage}\nSignature Valid: ${isSignatureValid}`,
-      )
+      publicKey = await generateKeyPair(keyAlias)
+      Alert.alert("Key Pair Generated", `Public Key: ${publicKey}`)
     } catch (error) {
-      if (error instanceof Error) {
-        Alert.alert("Error", error.message)
-      }
+      showError(error)
+    }
+  }
+
+  const handleEncrypt = async () => {
+    try {
+      const message = "Hello World"
+      cipher = await encrypt(message, keyAlias)
+      Alert.alert("Message Encrypted", `Cipher: ${cipher}`)
+    } catch (error) {
+      showError(error)
+    }
+  }
+
+  const handleSign = async () => {
+    try {
+      const message = "Hello World"
+      signature = await sign(message, keyAlias)
+      Alert.alert("Message Signed", `Signature: ${signature}`)
+    } catch (error) {
+      showError(error)
+    }
+  }
+
+  const handleDecrypt = async () => {
+    try {
+      const decryptedMessage = await decrypt(cipher, keyAlias)
+      Alert.alert("Message Decrypted", `Decrypted Message: ${decryptedMessage}`)
+    } catch (error) {
+      showError(error)
+    }
+  }
+
+  const handleVerify = async () => {
+    try {
+      const message = "Hello World"
+      const isSignatureValid = await verify(signature, message, keyAlias)
+      Alert.alert("Signature Verified", `Is Valid: ${isSignatureValid}`)
+    } catch (error) {
+      showError(error)
+    }
+  }
+
+  const showError = (error: unknown) => {
+    if (error instanceof Error) {
+      Alert.alert("Error", error.message)
     }
   }
 
   return (
     <View style={styles.container}>
-      <Button title="Test Encryption Module" onPress={testEncryptionModule} />
+      <Button title="Generate Key Pair" onPress={handleGenerateKeyPair} />
+      <Button title="Encrypt Message" onPress={handleEncrypt} />
+      <Button title="Sign Message" onPress={handleSign} />
+      <Button title="Decrypt Message" onPress={handleDecrypt} />
+      <Button title="Verify Signature" onPress={handleVerify} />
     </View>
   )
 }
@@ -59,7 +83,7 @@ const EncryptionTestScreen: React.FC = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: "space-around",
     alignItems: "center",
   },
 })
