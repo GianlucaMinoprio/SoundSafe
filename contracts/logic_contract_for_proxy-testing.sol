90fab5ebbd305b77252f7867413c875f246d6991
laurickB <laurick002@gmail.com>
2023-11-18 00:11:09 +0100

    h

---
+++ contracts/logic_contract_for_proxy-testing.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SimpleStorage {
+    uint256 private _value;
+
+    event ValueChanged(uint256 newValue);
+
+    // Fonction pour initialiser le contrat
+    function initialize(uint256 initialValue) public {
+        _value = initialValue;
+        emit ValueChanged(initialValue);
+    }
+
+    // Fonction pour mettre à jour la valeur
+    function setValue(uint256 newValue) public {
+        _value = newValue;
+        emit ValueChanged(newValue);
+    }
+
+    // Fonction pour lire la valeur
+    function getValue() public view returns (uint256) {
+        return _value;
+    }
+}
--- test/deploy_miteux.ts
+++ test/deploy_miteux.ts
@@ -8,13 +8,12 @@ async function main() {
     // Charger le contrat factory
     const Factory = await ethers.getContractFactory("GnosisSafeProxyFactory");
     const factory = await Factory.deploy();
-    await factory.deployed();
 
-    console.log("Factory deployed to:", factory.address);
+    console.log("Factory deployed to:", factory.runner.address);
 
     // Déployer un proxy via la factory
     // Remplacer `GnosisSafeProxy` par le contrat que vous voulez déployer via la factory
-    const proxy = await factory.createProxy("address_of_the_logic_contract", "0x");
+    const proxy = await factory.createProxy(process.env.FACTORY, "done");
     console.log("Proxy deployed to:", proxy.address);
 }
 
