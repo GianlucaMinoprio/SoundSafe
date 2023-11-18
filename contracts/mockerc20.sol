4e4ba3222a19057e5a415bcafc55d052d660adcb
tayron <michael.rousseau@epita.fr>
2023-11-18 16:41:20 +0100

    deploy contraacts

---
+++ contracts/mockerc20.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.8.20;
+contract MockERC20 {
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+    constructor(string memory _name, string memory _symbol, uint8 _decimals, uint256 _totalSupply) {
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+        totalSupply = _totalSupply;
+        balanceOf[msg.sender] = _totalSupply;
+    }
+    function transfer(address to, uint256 amount) external returns (bool) {
+        require(balanceOf[msg.sender] >= amount, "Insufficient balance");
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
+        return true;
+    }
+    function approve(address spender, uint256 amount) external returns (bool) {
+        allowance[msg.sender][spender] = amount;
+        return true;
+    }
+    function transferFrom(address from, address to, uint256 amount) external returns (bool) {
+        require(balanceOf[from] >= amount, "Insufficient balance");
+        require(allowance[from][msg.sender] >= amount, "Insufficient allowance");
+        balanceOf[from] -= amount;
+        balanceOf[to] += amount;
+        allowance[from][msg.sender] -= amount;
+        return true;
+    }
+}
---
+++ test/moduledeploy.js
@@ -0,0 +1,16 @@
+const {ethers} = require("hardhat");
+
+describe("Test", function () {
+   before(async function () {
+      signers = await ethers.getSigners();
+      cont = await ethers.getContractFactory("MockERC20");
+      erc = await cont.deploy("Mock", "MCK", 18, "1000000000000000000000000000000000000");
+      // cont = await ethers.getContractFactory("TokenSwap");
+      //
+      // tokenswap = await cont.deploy( erc.address);
+   });
+
+   it ("Should deploy", async function () {
+      console.log( await erc.address);
+   })
+});
