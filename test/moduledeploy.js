fdbd764492920cfc37543d69bf39ee17dd1f6371
tayron <michael.rousseau@epita.fr>
2023-11-18 18:04:48 +0100

    fine

--- test/moduledeploy.js
+++ test/moduledeploy.js
@@ -3,8 +3,8 @@ const {ethers} = require("hardhat");
 describe("Test", function () {
    before(async function () {
       signers = await ethers.getSigners();
-      cont = await ethers.getContractFactory("TokenSwap");
-      erc = await cont.deploy("TokenSwap");
+      cont = await ethers.getContractFactory("MockERC20");
+      erc = await cont.deploy("Mock", "MCK", 18, "1000000000000000000000000000000000000");
       // cont = await ethers.getContractFactory("TokenSwap");
       //
       // tokenswap = await cont.deploy( erc.address);
